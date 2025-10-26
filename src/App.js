faef5a2c1de1c3e010858e19295042684cbb01a6
Anand krishnan <77412636+anamite@users.noreply.github.com>
2025-10-26 11:12:36 +0100

    Add routing and new pages to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Silk from './components/Silk';
 import Sparkles from './components/Sparkles';
 import HomePage from './components/HomePage';
+import NikkahPage from './components/NikkahPage';
+import ReceptionPage from './components/ReceptionPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Silk
-        speed={10}
-        scale={1}
-        color="#59241c"
-        noiseIntensity={0.8}
-        rotation={0.3}
-      />
-      <Sparkles />
-      
-      <div className="site" role="main">
-        <HomePage />
+    <Router>
+      <div className="App">
+        <Silk
+          speed={10}
+          scale={1}
+          color="#59241c"
+          noiseIntensity={0.8}
+          rotation={0.3}
+        />
+        <Sparkles />
+        
+        <div className="site" role="main">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/nikkah" element={<NikkahPage />} />
+            <Route path="/reception" element={<ReceptionPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
